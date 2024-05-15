a7fc7d6fdb6b06e95648ae48836cac328a3c7ad7
LeoKrasowski <leokrasowski@gmail.com>
2024-05-15 22:00:12 +0200

    added functionally to conclusion each articles

--- src/main/java/com/example/demo/controllers/BlogController.java
+++ src/main/java/com/example/demo/controllers/BlogController.java
@@ -1,12 +1,14 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.example.demo.models.Post;
 import com.example.demo.repo.PostRepository;
 
@@ -35,4 +37,17 @@ public class BlogController {
 		postRepository.save(post);
     	return "redirect:/blog";
     }
+	
+	@GetMapping("/blog/{id}")
+	public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+		if(!postRepository.existsById(id)) {
+			return "redirect:/blog";
+		}
+		
+		Optional<Post> post = postRepository.findById(id);
+		ArrayList<Post> res = new ArrayList<>();
+		post.ifPresent(res::add);
+		model.addAttribute("post", res);
+		return "blog-details";
+	}
 }
